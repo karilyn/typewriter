1fbb09e5583aea19aff261481c4579d3e3253c2d
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-01-23 11:07:12 -0800

    Remove \n from within the string

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,12 @@
 // add a newline character to output after the string
-const sentence = "hello there from KK at lighthouse labs\n";
+const string = "hello there from KK at lighthouse labs\n";
+// add a newline char at the end of each string 
+const output = string + '\n'
 
 // name a delay variable
 let delay = 0;
 // loop through each character in the sentence
-for (const char of sentence) {
+for (const char of string) {
   setTimeout(() => {
   process.stdout.write(char);
   // the interval time is now the delay time
